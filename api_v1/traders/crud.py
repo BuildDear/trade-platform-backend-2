3e9d0f8153f72a832607a5b8c34ae4e3d99f2e26
marik <letschuka@gmail.com>
2024-04-10 18:51:38 +0300

    remove useless comments

--- api_v1/traders/crud.py
+++ api_v1/traders/crud.py
@@ -7,36 +7,25 @@ from api_v1.traders.schemas import TraderCreate, Trader
 async def get_traders(session: AsyncSession) -> list[Trader]:
     """
     Retrieves all traders from the database asynchronously.
-
-    :param session: Asynchronous session object.
-    :return: List of traders retrieved from the database.
     """
-    stat = select(Trader, Trader.id).order_by(Trader.id)  # Constructing a SELECT query
-    result: Result = await session.execute(stat)  # Executing the query
-    traders = result.scalars().all()  # Extracting and converting traders into a list
+    stat = select(Trader, Trader.id).order_by(Trader.id)
+    result: Result = await session.execute(stat)
+    traders = result.scalars().all()
     return list(traders)
 
 
 async def get_trader(session: AsyncSession, trader_id: int) -> Trader | None:
     """
     Retrieves a trader by ID from the database asynchronously.
-
-    :param session: Asynchronous session object.
-    :param trader_id: ID of the trader to retrieve.
-    :return: Retrieved trader object or None if not found.
     """
-    return await session.get(Trader, trader_id)  # Retrieving a trader by ID
+    return await session.get(Trader, trader_id)
 
 
 async def create_trader(session: AsyncSession, trader_in: TraderCreate) -> Trader:
     """
     Creates a new trader in the database asynchronously.
-
-    :param session: Asynchronous session object.
-    :param trader_in: Data of the trader to be created.
-    :return: Newly created trader object.
     """
-    trader = Trader(**trader_in.model_dump())  # Creating a new Trader object
-    session.add(trader)  # Adding the new trader to the session
-    await session.commit()  # Committing the transaction
-    return trader  # Returning the newly created trader object
+    trader = Trader(**trader_in.model_dump())
+    session.add(trader)
+    await session.commit()
+    return trader
