ae97e0aa7a56b8e3c7a2c878c4faa72da936ba3d
marik <letschuka@gmail.com>
2024-04-15 12:24:02 +0300

    add validation pass

--- api_v1/auth/utils.py
+++ api_v1/auth/utils.py
@@ -37,3 +37,13 @@ def hash_password(
     salt = bcrypt.gensalt()
     pwd_bytes: bytes = password.encode()
     return bcrypt.hashpw(pwd_bytes, salt)
+
+
+def validate_password(
+    password: str,
+    hashed_password: bytes,
+) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password,
+    )
