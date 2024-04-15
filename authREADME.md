afa33c79385e2a6e4afd55a9cf364408f9368e00
marik <letschuka@gmail.com>
2024-04-15 12:07:01 +0300

    update auth settings

--- authREADME.md
+++ authREADME.md
@@ -6,6 +6,6 @@ openssl genrsa -out jwt-private.pem 2048
 ```
 
 ```shell
-# Extract the public key from the key pair, which can be used in a certificate
-openssl rsa -in jwt-private.pem -outform PEM -pubout -out jwt-public.pem
+# Extract the jwt-public key from the key pair, which can be used in a certificate
+openssl rsa -in jwt-private.pem -outform PEM -pubout -out jwt-jwt-public.pem
 ```
--- core/config.py
+++ core/config.py
@@ -1,7 +1,17 @@
 from pathlib import Path
+
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
 BASE_DIR = Path(__file__).parent.parent
+
+
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "wt-public"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 15
+    # access_token_expire_minutes: int = 3
 
 
 class Setting(BaseSettings):
