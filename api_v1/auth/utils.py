39cd3da8a75db72b4ac3ceef222c9e4098ac64cc
marik <letschuka@gmail.com>
2024-04-15 12:27:43 +0300

    add decoding jwt

--- api_v1/auth/utils.py
+++ api_v1/auth/utils.py
@@ -31,6 +31,18 @@ def encode_jwt(
     return encoded
 
 
+def decode_jwt(
+    token: str | bytes,
+    public_key: str = settings.auth_jwt.public_key_path.read_text(),
+    algorithm: str = settings.auth_jwt.algorithm,
+) -> dict:
+    decoded = jwt.decode(
+        token,
+        public_key,
+        algorithms=[algorithm],
+    )
+    return decoded
+
 def hash_password(
     password: str,
 ) -> bytes:
